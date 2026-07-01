a38d53d0895cc2633be4701abf1f0151c91fdecb
escanor1204 <krabososina@mail.com>
2026-07-01 22:10:28 +0300

    Do changes with person["wife"] to person.get("wife")

--- app/main.py
+++ app/main.py
@@ -11,14 +11,14 @@ def create_person_list(people: list[dict]) -> list[Person]:
     Person.people = {}
     person_list = []
     for person in people:
-        new_person = Person(person["name"], person["age"])
+        new_person = Person(person.get("name"), person.get("age"))
         person_list.append(new_person)
     for person in people:
         current_person = Person.people[person["name"]]
-        if "wife" in person and (person["wife"] is not None):
-            wife_object = Person.people[person["wife"]]
+        if "wife" in person and (person.get("wife") is not None):
+            wife_object = Person.people[person.get("wife")]
             current_person.wife = wife_object
-        if "husband" in person and (person["husband"] is not None):
-            husband_object = Person.people[person["husband"]]
+        if "husband" in person and (person.get("husband") is not None):
+            husband_object = Person.people[person.get("husband")]
             current_person.husband = husband_object
     return person_list
