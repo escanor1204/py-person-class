3a4e563cef2d6af9d64ea6fc9e34e3e50f96d6e9
escanor1204 <krabososina@mail.com>
2026-07-01 22:02:09 +0300

    Create Person Class

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: list[dict]) -> list[Person]:
+    Person.people = {}
+    person_list = []
+    for person in people:
+        new_person = Person(person["name"], person["age"])
+        person_list.append(new_person)
+    for person in people:
+        current_person = Person.people[person["name"]]
+        if "wife" in person and (person["wife"] is not None):
+            wife_object = Person.people[person["wife"]]
+            current_person.wife = wife_object
+        if "husband" in person and (person["husband"] is not None):
+            husband_object = Person.people[person["husband"]]
+            current_person.husband = husband_object
+    return person_list
